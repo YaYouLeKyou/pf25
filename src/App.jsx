eea37f5cfcf6c75dc66ff5f8fbe6f830734a95be
Yayou <yanes75@hotmail.fr>
2025-07-31 16:06:49 +0200

    App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
+import { BrowserRouter } from "react-router-dom";
 
-import './App.css'
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    
-      <div>Portefolio 2025</div>
-  )
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
